905bceab7a76d82a9375c473293aafb4c97be931
Giovanni <gc3kyp@virginia.edu>
2023-03-26 12:21:43 -0400

    we good

--- app.py
+++ app.py
@@ -40,3 +40,14 @@ def twoDMotion():
         xposition = request.form.get("xposition")
         yposition = request.form.get("yposition")
     return render_template('2dMotion.html', angle = angle, v = velocity, x = xposition, y = yposition)
+
+# @app.route("/centrpetalVelocity", methods=['GET', 'POST'])
+# def oneDMotion():
+#     acceleration = 10
+#     velocity = 5
+#     position = 1
+#     if request.method == 'POST':
+#         radius = request.form.get("radius")
+#         period = request.form.get("period")
+#         mass = request.form.get("mass")
+#     return render_template('centripetalVelocty.html', r = radius, T = period, m = mass)
